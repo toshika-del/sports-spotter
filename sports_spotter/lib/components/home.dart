b7811f1c66be576cde6ea39b0b70b3eca6b45f0d
Prateek Thakur <prateekthakur272@gmail.com>
2023-09-10 01:46:16 +0530

    adds pull to refresh

--- sports_spotter/lib/components/home.dart
+++ sports_spotter/lib/components/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:liquid_pull_to_refresh/liquid_pull_to_refresh.dart';
 import 'package:sports_spotter/api/posts.dart';
 import 'package:sports_spotter/widgets/event_card.dart';
 
@@ -13,38 +14,48 @@ class _HomeState extends State<Home> {
   var response = fetchPosts();
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: FutureBuilder(
-          future: response,
-          builder: ((context, snapshot) {
-            if (snapshot.hasData) {
-              if (snapshot.data!.isEmpty) {
-                return Text(
-                  'There is nothing to show',
-                  style: TextStyle(
-                      fontSize: 18,
-                      color: Colors.blue.shade600,
-                      fontWeight: FontWeight.bold),
-                );
+    return LiquidPullToRefresh(
+      color: Colors.blue.shade700,
+      showChildOpacityTransition: false,
+      onRefresh: () async {
+        setState(() {
+          response = fetchPosts();
+        });
+        return;
+      },
+      child: Center(
+        child: FutureBuilder(
+            future: response,
+            builder: ((context, snapshot) {
+              if (snapshot.hasData) {
+                if (snapshot.data!.isEmpty) {
+                  return Text(
+                    'There is nothing to show',
+                    style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.blue.shade600,
+                        fontWeight: FontWeight.bold),
+                  );
+                }
+                final data = snapshot.data!;
+                return ListView.builder(
+                    itemCount: data.length,
+                    itemBuilder: (context, index) {
+                      return EventCard(
+                          image: NetworkImage(data[index]['image_url']),
+                          title: data[index]['title'],
+                          description: data[index]['description']);
+                    });
               }
-              final data = snapshot.data!;
-              return ListView.builder(
-                  itemCount: data.length,
-                  itemBuilder: (context, index) {
-                    return EventCard(
-                        image: NetworkImage(data[index]['image_url']),
-                        title: data[index]['title'],
-                        description: data[index]['description']);
-                  });
-            }
-            return Text(
-              'Some error occured',
-              style: TextStyle(
-                  fontSize: 18,
-                  color: Colors.red.shade400,
-                  fontWeight: FontWeight.bold),
-            );
-          })),
+              return Text(
+                'Some error occured',
+                style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.red.shade400,
+                    fontWeight: FontWeight.bold),
+              );
+            })),
+      ),
     );
   }
 }
