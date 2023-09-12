ea870935b48f76e67617f5f8b232a7c14a94c6db
Prateek Thakur <prateekthakur272@gmail.com>
2023-09-12 23:40:42 +0530

    adds auth methods

---
+++ sports_spotter_api/core/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    
+    class Meta(object):
+        model = User
+        fields = ('username', 'password', 'email')
--- sports_spotter_api/core/views.py
+++ sports_spotter_api/core/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+from rest_framework.parsers import JSONParser
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+from .serializers import UserSerializer
+from rest_framework import status
+
+@api_view(['POST'])
+def login(request):
+    data = JSONParser().parse(request)
+    print(data)
+    user = get_object_or_404(User, username=data['username'])
+    if not user.check_password(data['password']):
+        return JsonResponse({"error":"wrong password"},status=status.HTTP_403_FORBIDDEN)
+    print(user.has_usable_password())
+    token, created = Token.objects.get_or_create(user=user)
+    serializer = UserSerializer(user)
+    return JsonResponse({"token":token.key, "user":serializer.data})
+
+@api_view(['POST'])
+def logout(request):
+    return Response({})
+
+@api_view(['POST'])
+def register(request):
+    data = JSONParser().parse(request)
+    serializer = UserSerializer(data=data)
+    if serializer.is_valid():
+        serializer.save()
+        user = User.objects.get(username=data['username'])
+        user.set_password(data['password'])
+        user.save()
+        token = Token.objects.get(user=user)
+        return JsonResponse({"token":token.key, "user":serializer.data})
+    return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+def get_user(request):
+    data = JSONParser().parse(request)
+    try:
+        token = Token.objects.get(key=data['token'])
+        user = UserSerializer(token.user)
+        return Response(user.data)
+    except:
+        return JsonResponse({"error":"invalid token"})
