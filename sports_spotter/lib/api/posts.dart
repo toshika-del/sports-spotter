ea0c360c9aaf1b727dea69ac2b8a8a21498d3518
Prateek Thakur <prateekthakur272@gmail.com>
2023-09-14 02:02:26 +0530

    change base url for platforms

--- sports_spotter/lib/api/posts.dart
+++ sports_spotter/lib/api/posts.dart
@@ -1,11 +1,13 @@
 import 'package:http/http.dart' as http;
 import 'package:sports_spotter/constants.dart';
 import 'dart:convert';
+import 'dart:io' show Platform;
 
 Future<List?> fetchPosts() async {
   const headers = {'Authorization': 'Token $token'};
-  final response =
-      await http.get(Uri.parse('$baseUrl/posts/'), headers: headers);
+  final response = await http.get(
+      Uri.parse('${Platform.isAndroid ? baseUrl : baseUrlIOS}/posts/'),
+      headers: headers);
   if (response.statusCode == 200) {
     final List<dynamic> data = json.decode(response.body)['data'];
     final content = data.map((item) => item['attributes']).toList();
