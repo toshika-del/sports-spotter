fffa374f925eed492ba9d2248b8f2697fdfc19ae
Prateek Thakur <prateekthakur272@gmail.com>
2023-09-14 02:20:45 +0530

    change url

--- sports_spotter/lib/api/alerts.dart
+++ sports_spotter/lib/api/alerts.dart
@@ -6,7 +6,7 @@ import 'dart:convert';
 Future<List?> fetchAlerts() async {
   const headers = {'Authorization': 'Token $token'};
   final response = await http.get(
-      Uri.parse('${Platform.isAndroid ? baseUrl : baseUrlIOS}/posts/'),
+      Uri.parse('${Platform.isAndroid ? baseUrl : baseUrlIOS}/alerts/'),
       headers: headers);
   if (response.statusCode == 200) {
     final List<dynamic> data = json.decode(response.body)['data'];
