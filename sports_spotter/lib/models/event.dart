22ae1e273cdd02477866bbaf4f52ef3128d324e0
Prateek Thakur <prateekthakur272@gmail.com>
2023-09-21 13:54:26 +0530

    adds detail screen and models

---
+++ sports_spotter/lib/models/event.dart
@@ -0,0 +1,11 @@
+class EventModel {
+  final String id;
+  final String title;
+  final String description;
+  final String imgUrl;
+  const EventModel(
+      {required this.id,
+      required this.title,
+      required this.description,
+      required this.imgUrl});
+}
---
+++ sports_spotter/lib/screens/eventdetailscreen.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:sports_spotter/constants.dart';
+import 'package:sports_spotter/models/event.dart';
+
+class EventDetailScreen extends StatelessWidget {
+  final EventModel model;
+  const EventDetailScreen({super.key, required this.model});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        // appBar: AppBar(
+        //   title: Text(
+        //     model.title,
+        //     style: const TextStyle(fontWeight: FontWeight.bold),
+        //   ),
+        // ),
+        body: NestedScrollView(
+            headerSliverBuilder: (context, scrolled) {
+              return [
+                SliverAppBar(
+                    expandedHeight: 200,
+                    title: Text(model.title),
+                    floating: false,
+                    pinned: true,
+                    flexibleSpace: FlexibleSpaceBar(
+                      background: Image.network(
+                        model.imgUrl,
+                        fit: BoxFit.fill,
+                      ),
+                    ))
+              ];
+            },
+            body: SingleChildScrollView(
+              padding: paddingAll24,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    model.title,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 28,
+                    ),
+                  ),
+                  Text(
+                    model.description,
+                    style: const TextStyle(fontSize: 16),
+                  ),
+                  space16,
+                  Row(
+                    children: [
+                      Expanded(
+                          child: FilledButton(
+                              onPressed: () {},
+                              child: const Text('Create team'))),
+                      space16,
+                      Expanded(
+                          child: OutlinedButton(
+                              onPressed: () {}, child: const Text('Join team')))
+                    ],
+                  )
+                ],
+              ),
+            )));
+  }
+}
