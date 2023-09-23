76f5f449fd26ea3b4a0aeddf858f9553627410e1
Prateek Thakur <prateekthakur272@gmail.com>
2023-09-24 01:38:38 +0530

    debug

--- sports_spotter_api/sports_spotter_api/settings.py
+++ sports_spotter_api/sports_spotter_api/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 SECRET_KEY = os.environ.get('SECRET_KEY')
-DEBUG = int(os.environ.get('DEBUG'))
+DEBUG = True
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split()
 
 INSTALLED_APPS = [
