24009132bd0e455056e9ff6c99966a81b1355183
Prateek Thakur <prateekthakur272@gmail.com>
2023-09-25 21:06:19 +0530

    adds calandar

--- sports_spotter/lib/components/events_calandar.dart
+++ sports_spotter/lib/components/events_calandar.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:sports_spotter/constants.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class EventsCalandar extends StatefulWidget {
   const EventsCalandar({super.key});
@@ -8,8 +10,45 @@ class EventsCalandar extends StatefulWidget {
 }
 
 class _EventsCalandarState extends State<EventsCalandar> {
+  late DateTime _selectedDay;
+  late DateTime _focusedDay;
+  @override
+  void initState() {
+    _selectedDay = DateTime.now();
+    _focusedDay = DateTime.now();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: [
+        TableCalendar(
+            onDaySelected: (selected, focused) {
+              setState(() {
+                _selectedDay = selected;
+                _focusedDay = focused;
+              });
+            },
+            focusedDay: _focusedDay,
+            selectedDayPredicate: (day) {
+              return isSameDay(day, _selectedDay);
+            },
+            eventLoader: (day) {
+              return [];
+            },
+            calendarStyle: CalendarStyle(
+                selectedDecoration: BoxDecoration(
+                    shape: BoxShape.circle, color: Colors.blue.shade300),
+                todayDecoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: primaryColor,
+                ),
+                markerDecoration: BoxDecoration(color: primaryColor)),
+            firstDay: DateTime(2023),
+            lastDay: DateTime(2030)),
+        Expanded(child: ListView()),
+      ],
+    );
   }
 }
