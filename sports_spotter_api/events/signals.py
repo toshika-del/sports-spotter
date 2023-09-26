a868f2a9f71e0c814673da80ea557286f981e170
Prateek Thakur <prateekthakur272@gmail.com>
2023-09-26 11:29:04 +0530

    auto alert on result out

--- sports_spotter_api/events/signals.py
+++ sports_spotter_api/events/signals.py
@@ -1,6 +1,6 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import Event, Alert
+from .models import Event, Alert, Result
 
 @receiver(post_save, sender=Event, weak=False)
 def add_alert(sender, instance, created, **kwargs):
@@ -8,4 +8,12 @@ def add_alert(sender, instance, created, **kwargs):
         alert  = Alert.objects.create()
         alert.title = f'New event {instance.title}'
         alert.description = f'Last date to register is {instance.deactivate_date}'
+        alert.save()
+
+@receiver(post_save, sender=Result, weak=False)
+def add_alert(sender, instance, created, **kwargs):
+    if created:
+        alert  = Alert.objects.create()
+        alert.title = f'Result out for {instance.event.title}'
+        alert.description = f'Check out winner in results'
         alert.save()
