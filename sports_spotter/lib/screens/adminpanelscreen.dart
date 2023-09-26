57197fa0e50714f64612d09ae7339a3bc83996bb
Prateek Thakur <prateekthakur272@gmail.com>
2023-09-26 17:13:46 +0530

    adds bootom app bar

--- sports_spotter/lib/screens/adminpanelscreen.dart
+++ sports_spotter/lib/screens/adminpanelscreen.dart
@@ -14,10 +14,48 @@ class _AdminPanelScreenState extends State<AdminPanelScreen> {
     ..setJavaScriptMode(JavaScriptMode.disabled)
     ..loadRequest(Uri.parse('$baseUrl/admin/'));
   @override
+  void dispose() {
+    final WebViewCookieManager cookieManager = WebViewCookieManager();
+    cookieManager.clearCookies();
+    controller.clearLocalStorage();
+    controller.clearCache();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Management console')),
+      appBar: AppBar(
+        title: const Text('Management console'),
+      ),
       body: WebViewWidget(controller: controller),
+      bottomNavigationBar: SizedBox(
+        height: 72,
+        child: Wrap(
+          alignment: WrapAlignment.spaceEvenly,
+          children: [
+            IconButton(
+                onPressed: () async {
+                  if (await controller.canGoBack()) {
+                    controller.goBack();
+                  }
+                },
+                icon: const Icon(Icons.arrow_back)),
+            IconButton(
+                onPressed: () async {
+                  if (await controller.canGoForward()) {
+                    controller.goForward();
+                  }
+                },
+                icon: const Icon(Icons.arrow_forward)),
+            IconButton(
+                onPressed: () async {
+                  controller.reload();
+                },
+                icon: const Icon(Icons.refresh)),
+          ],
+        ),
+      ),
     );
   }
 }
